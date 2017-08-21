caee63927f8b84888e7b6b98bc8df1ecdd15eebf
noprom <tyee.noprom@qq.com>
2017-08-21 11:36:47 +0800

    add reading list controller

---
+++ src/main/java/com/huntdreams/readinglist/controller/ReadingListController.java
@@ -0,0 +1,41 @@
+package com.huntdreams.readinglist.controller;
+
+import com.huntdreams.readinglist.domain.Book;
+import com.huntdreams.readinglist.repository.ReadingListRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+/**
+ * Author: Tyeenoprom <tyee.noprom@qq.com>
+ * Date: 21/8/2017 11:09 AM
+ */
+@Controller
+@RequestMapping("/")
+public class ReadingListController {
+    private ReadingListRepository readingListRepository;
+
+    public ReadingListController(ReadingListRepository readingListRepository) {
+        this.readingListRepository = readingListRepository;
+    }
+
+    @RequestMapping(value = "/{reader}", method = RequestMethod.GET)
+    public String readersBooks(@PathVariable("reader") String reader, Model model) {
+        List<Book> readingList = readingListRepository.findByReader(reader);
+        if (readingList != null) {
+            model.addAttribute("books", readingList);
+        }
+        return "readingList";
+    }
+
+    @RequestMapping(value = "/{reader}", method = RequestMethod.POST)
+    public String addToReadingList(@PathVariable("reader") String reader, Book book) {
+        book.setReader(reader);
+        readingListRepository.save(book);
+        return "redirect:/{reader}";
+    }
+}
